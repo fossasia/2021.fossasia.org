ce39d7e9c7810775a95197ce0cd56c0f2052618a
Sopan <khoslasopan@gmail.com>
2016-07-05 00:09:51 +0530

    Adds plugin initials as prefix to twitter-outh disable script functions

--- js/twitterdisable.tweetscroll.js
+++ js/twitterdisable.tweetscroll.js
@@ -1,18 +1,18 @@
 jQuery(document).ready(function ($) {
     if($(".loklak_api").prop('checked')) {
-        wpltf_update_twitter_auth(true);
+        wpts_update_twitter_auth(true);
     }
 
     $(".loklak_api").live('change', function() {
         if($(this).is(':checked')){
-            wpltf_update_twitter_auth(true);
+            wpts_update_twitter_auth(true);
         }
         else {
-            wpltf_update_twitter_auth(false);
+            wpts_update_twitter_auth(false);
         }
     });
 
-    function wpltf_update_twitter_auth(arg) {
+    function wpts_update_twitter_auth(arg) {
         if (arg == true) {
             $(".consumer_key").prop('disabled', arg);
             $(".consumer_secret").prop('disabled', arg);
